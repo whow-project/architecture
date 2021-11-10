552625eff855fe28373b47832e6f2b48e16a0345
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2021-11-10 16:30:40 +0100

    Update configuration.py

--- ispra-lod-infrastructure/configuration.py
+++ ispra-lod-infrastructure/configuration.py
@@ -2,13 +2,13 @@
 
 #virtuoso
 triplestore_host = "127.0.0.1"
-triplestore_host_username = "andrea"
-triplestore_host_password = "t01:S06:d11_"
-triplestore_upload_folder = "/Users/andrea/Documents/CNR/ISPRA/virtuoso_test/upload"
-triplestore_delete_folder = "/Users/andrea/Documents/CNR/ISPRA/virtuoso_test/delete"
+triplestore_host_username = "username"
+triplestore_host_password = ""
+triplestore_upload_folder = "./upload"
+triplestore_delete_folder = "./delete"
 triplestore_url = "http://localhost:8891/sparql"
-triplestore_isql_path = "/Applications/Virtuoso Open Source Edition v7.2.app/Contents/virtuoso-opensource/bin"
-odbc_driver = "/Applications/Virtuoso Open Source Edition v7.2.app/Contents/virtuoso-opensource/lib/virtodbc.so"
+triplestore_isql_path = "/virtuoso-opensource/bin"
+odbc_driver = "/virtuoso-opensource/lib/virtodbc.so"
 triplestore_connection_retry_sleep = 60
 triplestore_connection_retry_attempts = 5
 triplestore_triples_upload_limit=1000
