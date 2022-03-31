0ae7084ac9c58d2810458960f82feba57567df6d
Giulio Settanta <giulio.settanta@gmail.com>
2022-03-31 14:32:42 +0200

    added function to convert only one file

--- ispra-lod-infrastructure/utf8_converter.py
+++ ispra-lod-infrastructure/utf8_converter.py
@@ -13,7 +13,7 @@ class UTF8Converter():
     def __init__(self, input_dir, output_dir):
     	self.__input_dir = input_dir
     	self.__output_dir = output_dir
-    	
+
     def convert(self):
         #fileext = "csv" #estensione del file da processare e codificare in UTF-8
         fileext = "*" # * ==> tutto quello che trova in input-toclean ... estensione del file da processare e codificare in UTF-8
@@ -60,5 +60,35 @@ class UTF8Converter():
 #            else:
  #               print("File %s already exists in UTF-8 encoding."%(justnomefile))
 
+
+    def convert_single_file(self, file_to_convert):
+    #as convert, but takes only one file
+
+        detector = UniversalDetector()
+        filename = os.path.join(self.__input_dir,file_to_convert)
+
+        print (filename.ljust(100))
+
+        posnomefile=filename.rfind("/") #LINUX ...
+        justnomefile=filename[posnomefile+1:]
+            
+        filenameclean=str(self.__output_dir)+'/'+justnomefile
+        detector.reset()
+
+        with open(filename, 'rb') as infile:
+            for line in infile:
+                detector.feed(line)
+                if detector.done: 
+                    break
+            detector.close()
+            #print (detector.result)
+            encodingfrom = detector.result['encoding']
+            print(encodingfrom)
+        
+        with open(filename, mode="r", encoding=encodingfrom) as fr:
+            data = fr.read()
+            
+        with open(filenameclean, mode="w",encoding='UTF-8') as fw:
+            fw.write(data)
     
 
