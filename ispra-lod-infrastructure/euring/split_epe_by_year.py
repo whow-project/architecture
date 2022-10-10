f310c09619fd261d02706f902036e24726e0fbb3
Giulio Settanta <giulio.settanta@gmail.com>
2022-10-10 15:55:48 +0200

    Split process enabled for different source files

--- ispra-lod-infrastructure/euring/split_epe_by_year.py
+++ ispra-lod-infrastructure/euring/split_epe_by_year.py
@@ -8,13 +8,15 @@ from tqdm import tqdm
 def split_epe_by_year(csv_file):
     print ('Reading', csv_file.name, '...')
     df_csv_large = pd.read_csv(csv_file.name, delimiter=';')
-    #csv_data_path = csv_file.name.replace(csv_file.name.split('/')[-1],'')
+    csv_data_name = (csv_file.name.split('/')[-1]).split('.')[0]
+    year_cols = [col for col in df_csv_large.columns if 'YEAR' in col]
+
     
-    for yy in tqdm(range(1900, 2022)):
-        #print ('Processing year', yy, '...')
-        df_years = df_csv_large[df_csv_large['YEAR'] == yy]
+    for yy in (range(1900, 2022)):
+        print ('Processing year', yy, '...')
+        df_years = df_csv_large[df_csv_large[year_cols[0]] == yy]
         if (not(df_years.empty)):
-            df_years.to_csv('data/euring/v2/dirtydata/epe_token_' + str(yy) + '.csv', index=None, sep=';')
+            df_years.to_csv('data/euring/v2/dirtydata/' + csv_data_name + '_' + str(yy) + '.csv', index=None, sep=';')
         del df_years
 
 
