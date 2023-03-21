35838255fa0f967e369d864640b4732763804c72
sgiulio70 <giulio.settanta@gmail.com>
2023-03-21 18:33:52 +0100

    Change datatype to preserve istat code

--- ispra-lod-infrastructure/pest/split_pest_by_year.py
+++ ispra-lod-infrastructure/pest/split_pest_by_year.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def split_pest_by_year(csv_file):
     print ('Reading', csv_file.name, '...')
-    df_csv_large = pd.read_csv(csv_file.name, delimiter=';')
+    df_csv_large = pd.read_csv(csv_file.name, delimiter=';', dtype={'codice_regione': 'str', 'codice_comune': 'str'})
     csv_data_name = (csv_file.name.split('/')[-1]).split('.')[0]
     year_cols = [col for col in df_csv_large.columns if 'anno' in col]
 
