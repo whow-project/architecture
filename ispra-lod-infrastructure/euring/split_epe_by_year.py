86db8f42a4436d54e82123740c9e400f0dcebbb2
sgiulio70 <giulio.settanta@gmail.com>
2023-04-28 09:48:21 +0200

    print only valid years

--- ispra-lod-infrastructure/euring/split_epe_by_year.py
+++ ispra-lod-infrastructure/euring/split_epe_by_year.py
@@ -13,9 +13,9 @@ def split_epe_by_year(csv_file):
 
     
     for yy in (range(1900, 2022)):
-        print ('Processing year', yy, '...')
         df_years = df_csv_large[df_csv_large[year_cols[0]] == yy]
         if (not(df_years.empty)):
+            print ('Processing year', yy, '...')
             df_years.to_csv('data/euring/v2/dirtydata/' + csv_data_name + '_' + str(yy) + '.csv', index=None, sep=';')
         del df_years
 
