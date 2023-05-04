914e0b17822213c85d3b0476cd629ed3c00e6c27
sgiulio70 <giulio.settanta@gmail.com>
2023-05-04 18:05:49 +0200

    Associate seas

--- ispra-lod-infrastructure/ostreopsis/Make_Ostreopsis_csv.py
+++ ispra-lod-infrastructure/ostreopsis/Make_Ostreopsis_csv.py
@@ -20,30 +20,59 @@ def aggregate_files(dpath, foutput):
 	df_sample.to_csv(foutput, sep=';', index=None)
 
 
-def associate_istat(file_istat, file_samples, foutput):
+def associate_istat_and_seas(file_istat, file_seas, file_samples):
 
 	'''
 	Associate each sampling with the ISTAT code of the nearest city
 	'''
 
-	print ('Associating records to ISTAT code ...')
 	#Read ISTAT shapefile
 	zipfile = file_istat+"!Limiti01012020/Com01012020/Com01012020_WGS84.shp"
 	gdf_shf = gpd.read_file(zipfile)
 	gdf_shf = gdf_shf.to_crs(epsg=32632)
+
+	#Read Med Sea file
+	medzipfile = file_seas+"!med_only.shp"
+	gdf_sea = gpd.read_file(medzipfile)
+	gdf_sea_ita = gdf_sea[gdf_sea['Country']=='IT']
+	gdf_sea_ita = gdf_sea_ita.to_crs(epsg=32632)
+	sea_dict = {'MAD': 'Adriatic Sea', 'MIC': 'Ionian Sea', 'MWE': 'Western Mediterranean Sea'}
+	gdf_sea_ita['Seaname'] = gdf_sea_ita['Subregion'].map(sea_dict)
 
 	#Read samples file
 	df_sample = pd.read_csv(file_samples, sep=';')
 	gdf_sample = gpd.GeoDataFrame(df_sample, crs=4326, geometry=gpd.points_from_xy(df_sample.LONG, df_sample.LAT))
 	gdf_sample = gdf_sample.to_crs(epsg=32632)
 
-	#Join geodataframes according to min distance
+	print ('Associating records to ISTAT code ...')
+	#Join ISTAT and samples geodataframes according to min distance
 	gdf_joined=gpd.sjoin_nearest(gdf_sample, gdf_shf)
 	try:
 		gdf_joined= gdf_joined.drop(['index_right','COD_RIP', 'COD_REG', 'COD_PROV', 'COD_CM', 'COD_UTS', 'PRO_COM', 'COMUNE', 'COMUNE_A', 'CC_UTS', 'Shape_Area', 'Shape_Leng', 'geometry'], axis=1)
 	except KeyError:
 		gdf_joined= gdf_joined.drop(['index_right','COD_RIP', 'COD_REG', 'COD_PROV', 'COD_CM', 'COD_UTS', 'PRO_COM', 'COMUNE', 'COMUNE_A', 'CC_UTS', 'SHAPE_AREA','SHAPE_LENG', 'SHAPE_LEN', 'geometry'], axis=1)
-	gdf_joined.to_csv(foutput, sep=';', index=None)
+	diff_record = int(len(df_sample)) - int(len(gdf_joined))
+	if (diff_record != 0):
+		print ("I'm missing", diff_record, 'records!')
+	foutname = file_samples.split('.')[0]+'_withISTATcode.csv'
+	gdf_joined.to_csv(foutname, sep=';', index=None)
+	print ('Created', foutname)
+
+
+	print ('Associating records to Seas ...')
+	#Join Seas and joined geodataframes according to min distance
+	gdf_joined.reset_index(inplace=True)
+	gdf_joined = gpd.GeoDataFrame(gdf_joined, crs=4326, geometry=gpd.points_from_xy(gdf_joined.LONG, gdf_joined.LAT))
+	gdf_joined = gdf_joined.to_crs(epsg=32632)
+	#Join geodataframes according to min distance
+	gdf_joined_sea=gpd.sjoin_nearest(gdf_joined, gdf_sea_ita)
+	gdf_joined_sea= gdf_joined_sea.drop(['Country', 'Area_km2', 'Type', 'geometry', 'index_right', 'Subregion'], axis=1)
+	diff_record = int(len(gdf_joined)) - int(len(gdf_joined_sea))
+	if (diff_record != 0):
+		print ("I'm missing", diff_record, 'records!')
+	foutname = file_samples.split('.')[0]+'_withISTATcode_withSeas.csv'
+	gdf_joined_sea.to_csv(foutname, sep=';', index=None)
+	print ('Created', foutname)
 
 	#Plot map
 	gdf_shf['coords'] = gdf_shf['geometry'].apply(lambda x: x.representative_point().coords[:])
@@ -77,8 +106,8 @@ if __name__ == '__main__':
 	#Modify these paths according to file location and desired output
 	data_path = 'data/ostreopsis/csv' #path with original csv files
 	file_output = 'data/ostreopsis/v2/dirtydata/Ostreopsis_Ovata_AllRegions.csv'
-	file_output_istat = 'data/ostreopsis/v2/dirtydata/Ostreopsis_Ovata_AllRegions_withISTATcode.csv'
 	file_comuni_istat = 'istat/Limiti01012020.zip'
+	file_mari = 'ostreopsis/med_only.zip'
 
 	aggregate_files(data_path, file_output)
-	associate_istat(file_comuni_istat, file_output, file_output_istat)
+	associate_istat_and_seas(file_comuni_istat, file_mari, file_output)
