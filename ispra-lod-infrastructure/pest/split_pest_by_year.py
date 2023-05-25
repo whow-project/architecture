44ce1d71fc60d74d950224bba983c9f9208ca5a6
sgiulio70 <giulio.settanta@gmail.com>
2023-05-25 14:47:22 +0200

    Manage with strings only, add double quotes

--- ispra-lod-infrastructure/pest/split_pest_by_year.py
+++ ispra-lod-infrastructure/pest/split_pest_by_year.py
@@ -1,20 +1,20 @@
 # USAGE: python3 split_pest_by_year.py <path/csv_file.csv>
 
-import sys
+import sys, csv
 import pandas as pd
 
 def split_pest_by_year(csv_file):
     print ('Reading', csv_file.name, '...')
-    df_csv_large = pd.read_csv(csv_file.name, delimiter=';', dtype={'codice_regione': 'str', 'codice_comune': 'str'})
+    df_csv_large = pd.read_csv(csv_file.name, delimiter=';', dtype=str)
     csv_data_name = (csv_file.name.split('/')[-1]).split('.')[0]
     year_cols = [col for col in df_csv_large.columns if 'anno' in col]
 
     
     for yy in (range(1900, 2022)):
-        df_years = df_csv_large[df_csv_large[year_cols[0]] == yy]
+        df_years = df_csv_large[df_csv_large[year_cols[0]] == str(yy)]
         if (not(df_years.empty)):
             print ('Processing year', yy, '...')
-            df_years.to_csv('data/pest/v2/dirtydata/' + csv_data_name + '_' + str(yy) + '.csv', index=None, sep=';')
+            df_years.to_csv('data/pest/v2/dirtydata/' + csv_data_name + '_' + str(yy) + '.csv', index=None, quoting=csv.QUOTE_NONNUMERIC, quotechar='"', sep=';')
         del df_years
 
 
