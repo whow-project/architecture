b4f65bad69923e90655332fc44a1d25b911ae603
sgiulio70 <giulio.settanta@gmail.com>
2023-11-17 13:23:08 +0100

    Fixed bug for utf8

--- ispra-lod-infrastructure/bathw/split_bathw_by_yy.py
+++ ispra-lod-infrastructure/bathw/split_bathw_by_yy.py
@@ -31,7 +31,7 @@ def split_by_year(csv_file, output_folder):
     #Align type with SKOS
     df_static['specialisedZoneType'] = df_static['specialisedZoneType'].apply(lambda x: str(x).replace('BathingWater', '_bathing_water'))
 
-    df_static.to_csv(os.path.join(output_folder, csv_data_name + '_static.csv'), index=None, quoting=csv.QUOTE_ALL, quotechar='"', sep=';', encoding='utf-8-sig')
+    df_static.to_csv(os.path.join(output_folder, csv_data_name + '_static.csv'), index=None, quoting=csv.QUOTE_ALL, quotechar='"', sep=';', encoding='utf-8')
     del df_static
 
 
@@ -52,7 +52,7 @@ def split_by_year(csv_file, output_folder):
                 new_cols = [col for col in df_year.columns if col != 'quality'] + ['quality']
                 df_year = df_year[new_cols]
 
-        df_year.to_csv(os.path.join(output_folder, csv_data_name + '_' + str(yy) + '.csv'), index=None, quoting=csv.QUOTE_ALL, quotechar='"', sep=';', encoding='utf-8-sig')
+        df_year.to_csv(os.path.join(output_folder, csv_data_name + '_' + str(yy) + '.csv'), index=None, quoting=csv.QUOTE_ALL, quotechar='"', sep=';', encoding='utf-8')
         del df_year
 
 def associate_istat_code(file_istat, file_samples):
@@ -82,7 +82,7 @@ def associate_istat_code(file_istat, file_samples):
     foutname = file_samples.split('.')[0]+'_withISTATcode.csv'
     gdf_joined = pd.concat([gdf_joined, pd.DataFrame(data={"quality": [np.nan for kk in range(len(gdf_joined))]})]) #add 'quality' column
     gdf_joined = gdf_joined.dropna(axis=0, how='all')
-    gdf_joined.to_csv(foutname, sep=';', index=None, quoting=csv.QUOTE_ALL, quotechar='"', encoding='utf-8-sig')
+    gdf_joined.to_csv(foutname, sep=';', index=None, quoting=csv.QUOTE_ALL, quotechar='"', encoding='utf-8')
     print ('Created', foutname)
 
 
